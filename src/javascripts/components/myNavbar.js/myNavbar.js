15d654c48b54d32842758a9f074e9fd163bbfa65
Todd Spainhour <toddspainhour@gmail.com>
2020-04-18 12:30:15 -0500

    weapons and sectors navbar click events working

--- src/javascripts/components/myNavbar.js/myNavbar.js
+++ src/javascripts/components/myNavbar.js/myNavbar.js
@@ -2,19 +2,36 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 import sectorBuilder from '../sectorArea/sectorArea';
+import buildWeapons from '../weaponTypes/weaponTypes';
 
 const weapontypeDiv = $('#weapontype');
+const viewweaponDiv = $('#view-weapon');
+const sectorDiv = $('#print-sector-cards-here');
+
 const sectorsNavbarClickEvent = () => {
   $('#sectors-navbar-button').click((e) => {
     e.preventDefault();
     sectorBuilder.buildSectors();
     weapontypeDiv.addClass('hide');
+    viewweaponDiv.addClass('hide');
+    sectorDiv.removeClass('hide');
   });
 };
 
+const weaponsNavbarClickEvent = () => {
+  $('#weapons-navbar-button').click((e) => {
+    e.preventDefault();
+    console.error('inside the weapon button event');
+    buildWeapons.buildWeaponTypes();
+    weapontypeDiv.removeClass('hide');
+    viewweaponDiv.addClass('hide');
+    sectorDiv.addClass('hide');
+  });
+};
 
 const navbarClickEvents = () => {
   sectorsNavbarClickEvent();
+  weaponsNavbarClickEvent();
 };
 
 
