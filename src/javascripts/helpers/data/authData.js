966edaa3052c13551fefb70623348a95f7c9c6c5
Todd Spainhour <toddspainhour@gmail.com>
2020-04-19 21:15:10 -0500

    started splash page image...changed mind. needs own branch

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -3,12 +3,14 @@ import 'firebase/auth';
 
 const authDiv = $('#auth');
 const logoutButton = $('#navbar-logout-button');
+const buttonDiv = $('btn-dark');
 
 const checkLoginStatus = () => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       authDiv.addClass('hide');
       logoutButton.removeClass('hide');
+      buttonDiv.removeClass('hide');
     } else {
       authDiv.removeClass('hide');
       logoutButton.addClass('hide');
