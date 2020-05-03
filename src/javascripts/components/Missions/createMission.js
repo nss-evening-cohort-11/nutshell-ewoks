4397a53a622d7002e2e201a67aa2ca72fd770a2d
Todd Spainhour <toddspainhour@gmail.com>
2020-05-03 13:49:04 -0500

    mission enemy options displaying in drop down menu

--- src/javascripts/components/Missions/createMission.js
+++ src/javascripts/components/Missions/createMission.js
@@ -1,6 +1,6 @@
 import utils from '../../helpers/utils';
 import sectorDataComponent from '../../helpers/data/sectorData';
-// import sectorData from '../../helpers/data/sectorData';
+import enemyDataComponent from '../../helpers/data/enemyData';
 // import smash from '../../helpers/data/smash';
 
 const showFormToCreateMission = () => {
@@ -36,11 +36,22 @@ const showFormToCreateMission = () => {
         domString += `<option value=${sector.name}>${sector.name}</option>`;
       });
       domString += '</select>';
-      domString += '</div>';
-      domString += '<button type="button" class="btn btn-primary" id="submit-new-mission-form-button">Create New Mission</button>';
-      domString += '</form>';
 
-      utils.printToDom('create-new-mission-form-goes-here', domString);
+      domString += '<label for="">Pick a Mission Enemy</label>';
+      domString += '<select id="mission-enemy-drop-down">';
+      enemyDataComponent.getAllEnemies()
+        .then((enemies) => {
+          enemies.forEach((enemy) => {
+            domString += `<option value=${enemy.name}>${enemy.name}</option>`;
+            domString += '</div>';
+            domString += '<button type="button" class="btn btn-primary" id="submit-new-mission-form-button">Create New Mission</button>';
+            domString += '</form>';
+
+            utils.printToDom('create-new-mission-form-goes-here', domString);
+          });
+          domString += '</select>';
+        })
+        .catch((err) => console.error('problen with the getAllEnemies drop down menu', err));
     })
     .catch((err) => console.error('something went wrong inside your getSectors function inside the sectors dropdown menu', err));
 };
